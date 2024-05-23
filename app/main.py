bcce77b73232c088acf5129a211de7f55d21f1ee
Ivan Svyatykh <Zlativansvyat@mail.ru>
2024-05-23 21:52:41 +0500

    удалил ненужные импорты

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from nlp_controler import router as nlp_model_router
-from transcriptor import router as transcription_router
 
 app = FastAPI(title="RFA Ml backend server")
 
-app.include_router(transcription_router)
 app.include_router(nlp_model_router)
-
-
