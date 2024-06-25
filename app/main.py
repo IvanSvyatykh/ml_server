dd7ccc7e9cb7435e74e3b89d157595951c045866
Ivan Svyatykh <Zlativansvyat@mail.ru>
2024-06-25 18:06:31 +0500

    fix: docker работает

--- app/main.py
+++ app/main.py
@@ -5,3 +5,6 @@ from nlp_controler import router as nlp_model_router
 app = FastAPI(title="RFA Ml backend server")
 
 app.include_router(nlp_model_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app=app,host="0.0.0.0", port=8000)
